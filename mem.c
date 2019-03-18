35e57d40a30e31fb9af94ea57e1ae8b475ed1162
Nicolas LABBE <nicolaslabbe2000@gmail.com>
2019-03-18 20:51:02 +0100

    Commit #6 : commentaires

--- mem.c
+++ mem.c
@@ -41,23 +41,22 @@ void mem_init(void* mem, size_t taille)
 }
 
 void mem_show(void (*print)(void *, size_t, int)) {
-	size_t tailletotale=0;
+	size_t taillelue=0;
 	char c;
 	struct fb* mem = get_memory_adr();
 	struct fb* currentZL = PremZL;
 	size_t taille;
-	while (tailletotale < SizeMem) {
-		taille=*((size_t*)(mem));
-		if(mem==(void*)currentZL){
+	while (taillelue < SizeMem) {				// On lit tant qu'on a pas atteint la taille de la mémoire
+		taille=*((size_t*)(mem));							// On récupère la taille de la zone
+		if(mem==(void*)currentZL){						// si la zone est dans la liste libre
 			print(mem,taille,1);
 			currentZL = currentZL->next;
 		}
 		else{
 			print(mem,taille,0);
 		}
-		tailletotale += taille;
-		mem = (void*) mem + taille;
-		scanf("%c",&c);
+		taillelue += taille;								// on augmente la taille lue
+		mem = (void*) mem + taille;						// on avance dans la memoire
 	}
 }
 
@@ -70,57 +69,57 @@ void *mem_alloc(size_t taille) {
 
 	__attribute__((unused)) /* juste pour que gcc compile ce squelette avec -Werror */
 
-	struct fb *fb=mem_fit_fn(PremZL,taille + sizeof(size_t));
+	struct fb *fb=mem_fit_fn(PremZL,taille + sizeof(size_t));		// on récupère la zone libre
+
   if(fb == NULL){
 		return NULL;
 	}
-	size_t taillealign =  (taille + sizeof(size_t)) + ALIGNEMENT - (taille + sizeof(size_t))%ALIGNEMENT;
-	if((taille + sizeof(size_t))%ALIGNEMENT == 0) {
+
+	size_t taillealign =  (taille + sizeof(size_t)) + ALIGNEMENT - (taille + sizeof(size_t))%ALIGNEMENT; // on aligne la zone libre sur notre valeur d'alignement
+	if((taille + sizeof(size_t))%ALIGNEMENT == 0) {				// si elle était déjà alignée, on reréduit à cette valeur
 		taillealign -= ALIGNEMENT;
 	}
-	if(PremZL == fb) {
+
+	if(PremZL == fb) {																		// Cas où notre zone libre est la première
 		if (PremZL->next == NULL){
-			size_t t = PremZL->size;
+			size_t t = PremZL->size;													// si on a une seule zone libre, on crée une nouvelle première zone libre
 			PremZL = ((void*)PremZL + taillealign);
 			PremZL->size = t - taillealign;
 		}
-		else {
-			if(PremZL->size <= taillealign + ALIGNEMENT){
-				taillealign += ALIGNEMENT;
+		else {																							// si on a d'autres zones libres
+			if(PremZL->size <= taillealign + ALIGNEMENT){			// si on n'a pas la place pour créer une nouvelle zone libre
+				taillealign += ALIGNEMENT;											// on place dans le padding la place inutilisable
 				PremZL = PremZL->next;
 			}
 			else{
-				struct fb *tmp = PremZL->next;
+				struct fb *tmp = PremZL->next;									// on conserve bien la liste des zones libres
 				size_t t = PremZL->size;
-				PremZL = ((void*)PremZL + taillealign);
+				PremZL = ((void*)PremZL + taillealign);					// on crée une nouvelle zone libre de la nouvelle taille
 				PremZL->size = t - taillealign;
 				PremZL->next = tmp;
 			}
 
 		}
 	}
-	else{
+	else{																									// si on est pas sur la première zone libre
 		struct fb *currentZL = PremZL;
 		struct fb *suivZL ;
-		while (currentZL->next != NULL){
+		while (currentZL->next != NULL){										// on cherche la zone dans notre liste de zone libres
 			suivZL = currentZL->next;
-			if(fb == suivZL){
-				if(suivZL->next == NULL){
+			if(fb == suivZL){																	// quand on l'a trouvée
+				if(suivZL->next == NULL){												// on regarde si c'est la dernière
 					size_t t = suivZL->size;
-					suivZL = ((void*)suivZL + taillealign);
+					suivZL = ((void*)suivZL + taillealign);				// dans ce cas, on en crée une nouvelle
 					suivZL->size = t - taillealign;
 					currentZL->next = suivZL;
-					printf("sal");
 				}
 				else{
-					printf("%ld %ld", suivZL->size,taillealign + ALIGNEMENT);
-					if(suivZL->size >taillealign && suivZL->size <= taillealign + ALIGNEMENT){
-						printf("alo");
-						taillealign += ALIGNEMENT;
+					if(suivZL->size >taillealign && suivZL->size <= taillealign + ALIGNEMENT){ // si on n'a pas la place de créer une nouvelle zone libre
+						taillealign += ALIGNEMENT;									// On place dans la padding la place perdue
 						currentZL->next = suivZL->next;
 					}
 					else{
-						struct fb *tmp = suivZL->next;
+						struct fb *tmp = suivZL->next;							// sinon, on crée une nouvelle zone libre à cet emplacement
 						size_t t = suivZL->size;
 						suivZL = ((void*)suivZL + taillealign);
 						suivZL->size = t - taillealign;
@@ -133,24 +132,25 @@ void *mem_alloc(size_t taille) {
 			}
 		}
 	}
-	void* zo = (size_t*)fb;
-	*(size_t*)zo = taillealign;
-	return zo+sizeof(size_t);
+
+	void* zo = (size_t*)fb;																// on place dans un pointeur l'adresse de la zone libre
+	*(size_t*)zo = taillealign;														// on met a cette adresse la taille de la zone occupée
+	return zo+sizeof(size_t);															// on retourne l'adresse de la zone occupée, sans inclure les metadonnées
 }
 
 
 void mem_free(void* mem) {
-	mem = mem - sizeof(size_t);
+	mem = mem - sizeof(size_t);														// on recule pour acceder aux metadonnées
 	struct fb *currentZL = PremZL;
 	struct fb *memoire = mem;
-	memoire->size = *(size_t*)mem;
+	memoire->size = *(size_t*)mem;												// on place la taille de la zone dans sa structure
 
-	if(memoire<PremZL){
+	if(memoire<PremZL){																		// si on est avant la première zone libre, on devient la première ZL
 		PremZL = memoire;
 		PremZL->next = currentZL;
 	}
 	else{
-		while(memoire > currentZL->next){
+		while(memoire > currentZL->next){										// sinon, on cherche où elle se situe dans la lite
 			currentZL =  currentZL->next;
 		}
 		memoire->next = currentZL->next;
@@ -159,10 +159,10 @@ void mem_free(void* mem) {
 
 	currentZL = PremZL;
 	struct fb* suivZL;
-	while(currentZL->next != NULL){
+	while(currentZL->next != NULL){												// tant qu'on est pas à la fin de la liste de zones libres
 		suivZL = currentZL->next;
-		if((void*)currentZL + currentZL->size == currentZL->next){
-			currentZL->size += suivZL->size;
+		if((void*)currentZL + currentZL->size == currentZL->next){			// si deux zones libres se suivent
+			currentZL->size += suivZL->size;									// on les fusionne
 			currentZL->next = suivZL->next;
 		}
 		else{
